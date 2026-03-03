7a902e24ae3943c59b63ca5d05b894de61de40aa
UtterlyTerrible <hoarsekat@outlook.com>
2026-03-03 23:01:06 +1100

    Spice up docs

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -5,14 +5,12 @@ import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  // Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
     title: "Own your Codebase",
-    // Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
         Quill is extremely flexible, and divides its functionality between
@@ -22,7 +20,6 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: "Script with Power",
-    // Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
         Quill provides many different approaches to handling bootstrapping,
@@ -33,7 +30,6 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: "Enhance your Productivity",
-    // Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
         Quill is great for enhancing your productivity and changing the ways in
@@ -42,18 +38,21 @@ const FeatureList: FeatureItem[] = [
     ),
   },
 ];
-// title, svg, description
-/*
-<div className="text--center">
-  <Svg className={styles.featureSvg} role="img" />
-</div>
-*/
+
 function Feature({ title, description }: FeatureItem) {
   return (
-    <div className={clsx("col col--4")}>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
+    <div className={clsx("col col--4", "margin-bottom--lg")}>
+      <div className={styles.featureCard}>
+        <div className={styles.icon}>
+          <span className={styles.iconDot} />
+        </div>
+
+        <div className="text--center padding-horiz--md">
+          <Heading as="h3" className={styles.featureTitle}>
+            {title}
+          </Heading>
+          <p className={styles.featureText}>{description}</p>
+        </div>
       </div>
     </div>
   );
